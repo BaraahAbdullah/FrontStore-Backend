c83eb87a8ab28b8cb1b1a15ce2550638cc49de78
Baraah <57688502+BaraahAbdullah@users.noreply.github.com>
2022-12-05 19:07:19 +0300

    Update order.spec.ts

--- src/tests/models/order.spec.ts
+++ src/tests/models/order.spec.ts
@@ -3,16 +3,16 @@ import { OrderDBModel } from '../../models/order'
 const order = new OrderDBModel()
 
 describe('Order Model', () => {
-  it('should have a create method', () => {
-    expect(order.create).toBeDefined()
-  })
-   it('should have a show method', () => {
-    expect(order.show).toBeDefined()
-  })
+//   it('should have a create method', () => {
+//     expect(order.create).toBeDefined()
+//   })
+//    it('should have a show method', () => {
+//     expect(order.show).toBeDefined()
+//   })
 
-  // it('create method should create new order with new associated order_product', async () => {
-  //   const orderProduct = { product_id: 1, quantity: 1 }
-  //   const result = await order.create('active', 2, [orderProduct])
-  //   expect(result).toEqual([])
-  // })
+  it('create method should create new order with new associated order_product', async () => {
+    const orderProduct = { product_id: 1, quantity: 1 }
+    const result = await order.create('active', 2, [orderProduct])
+    expect(result).toEqual([])
+  })
 })
